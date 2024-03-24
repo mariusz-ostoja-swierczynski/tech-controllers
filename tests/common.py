a03c258b9c542b175d2d5f96fa1f8f90c111451f
Anarion <anarion80@gmail.com>
2024-03-24 14:47:57 +0100

    ✅ Add Tech API tests

---
+++ tests/common.py
@@ -0,0 +1,12 @@
+"""Common helpers."""
+import pathlib
+
+
+def load_fixture(filename: str, integration: str | None = None) -> str:
+    """Load a fixture."""
+    return get_fixture_path(filename, integration).read_text()
+
+
+def get_fixture_path(filename: str, integration: str | None = None) -> pathlib.Path:
+    """Get path of fixture."""
+    return pathlib.Path(__file__).parent.joinpath("fixtures", filename)
