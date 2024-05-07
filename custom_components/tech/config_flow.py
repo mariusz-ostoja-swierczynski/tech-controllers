c174ebb72477786d32874dae0ab6e0c8fdd29813
Anarion <anarion80@gmail.com>
2024-05-07 11:55:50 +0200

    :bug: remove migrations. Fix #89

--- custom_components/tech/config_flow.py
+++ custom_components/tech/config_flow.py
@@ -85,8 +85,8 @@ async def validate_input(hass: core.HomeAssistant, data):
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Tech Sterowniki."""
 
-    VERSION = 2
-    MINOR_VERSION = 3
+    VERSION = 3
+    MINOR_VERSION = 0
     # Pick one of the available connection classes in homeassistant/config_entries.py
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
