259a54fd3280e3c89fe028923a0c260b32aac16d
Dawid Rogaczewski <20227477+daroga0002@users.noreply.github.com>
2024-07-24 16:53:10 +0000

    just simple refactor

--- custom_components/tech/__init__.py
+++ custom_components/tech/__init__.py
@@ -35,7 +35,7 @@ async def async_setup(hass: HomeAssistant, config: dict):  # pylint: disable=unu
     return True
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Tech Controllers from a config entry."""
     _LOGGER.debug("Setting up component's entry.")
     _LOGGER.debug("Entry id: %s", str(entry.entry_id))
@@ -65,16 +65,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
 
